ca1fcbab39e39299972632eee797594067ce822b
MShreyansh916 <shreyu.medatati@gmail.com>
2025-06-17 14:10:42 -0700

    add greeter

---
+++ name_greeter.py
@@ -0,0 +1,40 @@
+"""
+Your First Interactive Python Program
+-------------------------------------
+Author:  <Shreyansh Medatati>
+Date:    <June 17, 2025>
+
+What to do:
+   1. Replace <Your Name> and <Today's Date>.
+   2. Follow the prompts in the terminal.
+   3. Customize the questions or add more input/output.
+"""
+
+# ---- 1. Main logic -------------------------------------------------
+def main() -> None:
+    """
+    This function asks the user for their name and responds.
+    """
+    # Ask the user for their name
+name = input ("what is your name?")
+
+
+    # Respond with a greeting
+greeting = input ("Hello " + str(name) + ", how are you doing?")
+   
+    # Optional challenge: Ask for age and respond
+age = input ( "What is your age?")
+age_num = int(age)
+until_25 = 25-age_num
+print("You have " + str(until_25) + " years until you are 25 years old.")
+# ---- 2. “Guard” to run main() when executed directly ---------------
+if __name__ == "__main__":
+    main()
+
+"""
+How to run: 
+1) Open Terminal (Ctrl + Shift + `)
+2) type: python week1_1.py
+3) Program should run! Let us know if it doesn't
+
+"""
